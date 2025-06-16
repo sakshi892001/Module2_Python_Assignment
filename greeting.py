34a222ec161aa296ab831606bc3dff544c13b5bb
sakshi892001 <sakshi892001@gmail.com>
2025-06-16 19:33:10 +0530

    Added greeting.py

---
+++ greeting.py
@@ -0,0 +1,4 @@
+# This program greets the user by their first and last name
+a=input("Enter your first name: ")
+b=input("Enter your last name: ")
+print(f"Hello, {a} {b}! Welcome to the Python Program.")   
